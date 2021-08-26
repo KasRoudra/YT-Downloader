1dfdfbc88460992be275a2fa7aa4cad346e69e10
KasRoudra <78908440+KasRoudra@users.noreply.github.com>
2021-08-26 09:03:00 +0600

    Removed bold colors

--- yd.rb
+++ yd.rb
@@ -1,3 +1,13 @@
+# YT-Downloader
+
+# Author       : KasRoudra
+# Github       : https://github.com/KasRoudra
+# Messenger    : https://m.me/KasRoudra
+# Email        : kasroudrakrd@gmail.com
+# Date         : 26-08-2021
+# Main Language: Ruby
+# Description  : Download any video or audio of youtube from command line
+
 black="\033[0;30m"
 red="\033[0;31m"
 green="\033[0;32m"
@@ -6,16 +16,6 @@ blue="\033[0;34m"
 purple="\033[0;35m"
 cyan="\033[0;36m"
 white="\033[0;37m"
-
-# Bold
-bblack="\033[1;30m"
-bred="\033[1;31m"
-bgreen="\033[1;32m"
-byellow="\033[1;33m"
-bblue="\033[1;34m"
-bpurple="\033[1;35m"
-bcyan="\033[1;36m"
-bwhite="\033[1;37m"
 
 logo="""
 """+green+"""╭╮╱╱╭┳━━━━╮╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╭╮
@@ -79,7 +79,7 @@ post = response.parsed_response
 status= "#{post['status']}"
 while not post==""
 if status==""
-    puts info+"Currently service not available.Try again later."
+    puts info+"Currently service not available. Server reached daily limit. Try again later."
     exit    
 elsif status == "true"
     break
